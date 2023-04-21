3d17ff8913c412ee78b8147e719eec4c89db678f
harshitpandey0426 <harshit.pandey@students.iiit.ac.in>
2023-04-21 15:57:37 +0530

    added carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './Layouts/NavbarAndFooter/Navbar';
 import ExploreTopBooks from './Layouts/HomePage/ExploreTopBooks';
+import Carousel from './Layouts/HomePage/Carousel';
+import ReturnBook from './Layouts/HomePage/ReturnBook';
 
 function App() {
   return (
     <div>
     <Navbar/>
     <ExploreTopBooks/>
+    <Carousel/>
     </div>
   );
 }
