40880ec032b63d49c0961e7820ebd14375312b24
harshitpandey0426 <harshit.pandey@students.iiit.ac.in>
2023-04-21 13:51:26 +0530

    added home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Layouts/NavbarAndFooter/Navbar';
+import ExploreTopBooks from './Layouts/HomePage/ExploreTopBooks';
 
 function App() {
   return (
+    <div>
     <Navbar/>
+    <ExploreTopBooks/>
+    </div>
   );
 }
 
