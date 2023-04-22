5ee4587038a428087b4ee223f3ab2ba92aaac49e
harshitpandey0426 <harshit.pandey@students.iiit.ac.in>
2023-04-22 17:40:50 +0530

    testing which all files are merged

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -1,2 +1,36 @@
-package com.luv2code.springbootlibrary.entity;public class Book {
+package com.luv2code.springbootlibrary.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+@Entity
+@Table(name = "Book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
 }
